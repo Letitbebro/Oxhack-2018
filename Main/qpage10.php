4477102adae007a352f78b37670c61116cf8cbc7
Jay Vachhani <44921281+TheJayVachhani@users.noreply.github.com>
2018-11-24 20:53:29 +0000

    Update qpage10.php
    Added Question and Answer

--- Main/qpage10.php
+++ Main/qpage10.php
@@ -6,11 +6,11 @@
 
 Check the answer to each multiple-coice question, and click on the "Send Form" button to submit the information.
 
-<P>1. Question bla bla<BR>
-<input type="radio" name="#" value="#">op1<BR>
-<input type="radio" name="#" value="#">op2<BR>
-<input type="radio" name="#" value="#">op3<BR>
-<input type="radio" name="#" value="#">op4<BR>
+<P>10. Which of the following is an aim of UK government macroeconomic policy?<BR>
+<input type="radio" name="#" value="#">A. A fall in the supply of money.<BR>
+<input type="radio" name="#" value="#">B. A stable labour force.<BR>
+<input type="radio" name="#" value="#">C. An increase in real GDP per head<BR>
+<input type="radio" name="#" value="#">D. Low interest rates<BR>
 </p>
 <br>
 <input type="submit" value="Send Form">
@@ -26,7 +26,7 @@ Finish!
 function checkAnswer() {
   var a= document.getElementByName("option");
   for (var i = 0, i < a.length; i++) {
-    if (a[i].value == "op1")
+    if (a[i].value == "C. An increase in real GDP per head")
     alert("You have selected the correct answer");
     //push(1).array
      
