a6ed0cb00ba19a2355782f9995bde65d681c4cdc
Jay Vachhani <44921281+TheJayVachhani@users.noreply.github.com>
2018-11-24 20:44:43 +0000

    Update qpage7.php
    Added Question and Answer

--- Main/qpage7.php
+++ Main/qpage7.php
@@ -6,11 +6,11 @@
 
 Check the answer to each multiple-coice question, and click on the "Send Form" button to submit the information.
 
-<P>1. Question bla bla<BR>
-<input type="radio" name="#" value="#">op1<BR>
-<input type="radio" name="#" value="#">op2<BR>
-<input type="radio" name="#" value="#">op3<BR>
-<input type="radio" name="#" value="#">op4<BR>
+<P>7. Deflation is:<BR>
+<input type="radio" name="#" value="#">A. A fall in the external value of the domestic currency.<BR>
+<input type="radio" name="#" value="#">B. A fall in the rate of inflation.<BR>
+<input type="radio" name="#" value="#">C. A loss in value of capital stock due to physical wear and tear.<BR>
+<input type="radio" name="#" value="#">D. A rise in the real value of money over time.<BR>
 </p>
 <br>
 <input type="submit" value="Send Form">
@@ -26,7 +26,7 @@ Next question
 function checkAnswer() {
   var a= document.getElementByName("option");
   for (var i = 0, i < a.length; i++) {
-    if (a[i].value == "op1")
+    if (a[i].value == "D. A rise in the real value of money over time.")
     alert("You have selected the correct answer");
     //push(1).array
      
