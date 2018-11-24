5838c6878051f92a24deb503d73507fedf4df365
Jay Vachhani <44921281+TheJayVachhani@users.noreply.github.com>
2018-11-24 20:36:07 +0000

    Update qpage6.php
    Added Question and Answer

--- Main/qpage6.php
+++ Main/qpage6.php
@@ -6,11 +6,11 @@
 
 Check the answer to each multiple-coice question, and click on the "Send Form" button to submit the information.
 
-<P>1. Question bla bla<BR>
-<input type="radio" name="#" value="#">op1<BR>
-<input type="radio" name="#" value="#">op2<BR>
-<input type="radio" name="#" value="#">op3<BR>
-<input type="radio" name="#" value="#">op4<BR>
+<P>6. Which of the following is a correct statement?(Fiscal policy refers to policies affecting government spending and taxation)<BR>
+<input type="radio" name="#" value="#">A. Fiscal policy can be used to affect both the level of economic activity and the pattern of economic activity<BR>
+<input type="radio" name="#" value="#">B. The Bank of England handles fiscal policy.<BR>
+<input type="radio" name="#" value="#">C. Fiscal policy is used only to affect the demand side of the economy<BR>
+<input type="radio" name="#" value="#">D. The government uses fiscal policy to control the exchange rate.<BR>
 </p>
 <br>
 <input type="submit" value="Send Form">
@@ -26,7 +26,7 @@ Next question
 function checkAnswer() {
   var a= document.getElementByName("option");
   for (var i = 0, i < a.length; i++) {
-    if (a[i].value == "op1")
+    if (a[i].value == "A. Fiscal policy can be used to affect both the level of economic activity and the pattern of economic activity")
     alert("You have selected the correct answer");
     //push(1).array
      
