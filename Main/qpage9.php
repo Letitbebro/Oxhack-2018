dd9f63b0cda803e11b70d4e958445f257254da85
Jay Vachhani <44921281+TheJayVachhani@users.noreply.github.com>
2018-11-24 20:51:16 +0000

    Update qpage9.php
    Added Question and Answer

--- Main/qpage9.php
+++ Main/qpage9.php
@@ -6,11 +6,11 @@
 
 Check the answer to each multiple-coice question, and click on the "Send Form" button to submit the information.
 
-<P>1. Question bla bla<BR>
-<input type="radio" name="#" value="#">op1<BR>
-<input type="radio" name="#" value="#">op2<BR>
-<input type="radio" name="#" value="#">op3<BR>
-<input type="radio" name="#" value="#">op4<BR>
+<P>9. Which of the following is most likely to reduce inflationary pressures in the UK economy?<BR>
+<input type="radio" name="#" value="#">A. An increase in labour productivity.<BR>
+<input type="radio" name="#" value="#">B. A fall in the value of the pound.<BR>
+<input type="radio" name="#" value="#">C. The emergence of a positive output gap.<BR>
+<input type="radio" name="#" value="#">D. A reduction in the rate of interest paid on mortgages.<BR>
 </p>
 <br>
 <input type="submit" value="Send Form">
@@ -26,7 +26,7 @@ Next question
 function checkAnswer() {
   var a= document.getElementByName("option");
   for (var i = 0, i < a.length; i++) {
-    if (a[i].value == "op1")
+    if (a[i].value == "A. An increase in labour productivity.")
     alert("You have selected the correct answer");
     //push(1).array
      
