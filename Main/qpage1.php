0423d9356e1a44d63cea1ce897104c76fa74a3d9
Jay Vachhani <44921281+TheJayVachhani@users.noreply.github.com>
2018-11-24 20:21:12 +0000

    Update qpage1.php

--- Main/qpage1.php
+++ Main/qpage1.php
@@ -7,10 +7,10 @@
 Check the answer to each multiple-coice question, and click on the "Send Form" button to submit the information.
 
 <P>1. Which one of the following is most likely to lead to greater labour productivity in an industry?<BR>
-<input type="radio" name="option" value="#">a. An increase in market demand<BR>
-<input type="radio" name="option" value="#">b. A fall in wages in the industry<BR>
-<input type="radio" name="option" value="#">c. An increase in the number of firms in the industry<BR>
-<input type="radio" name="option" value="#">d. An increase in capital investment in the industry<BR>
+<input type="radio" name="option" value="#">A. An increase in market demand<BR>
+<input type="radio" name="option" value="#">B. A fall in wages in the industry<BR>
+<input type="radio" name="option" value="#">C. An increase in the number of firms in the industry<BR>
+<input type="radio" name="option" value="#">D. An increase in capital investment in the industry<BR>
 </p>
 <br>
 <input type="submit" value="Send Form">
@@ -26,7 +26,7 @@ Next question
 function checkAnswer() {
   var a= document.getElementByName("option");
   for (var i = 0, i < a.length; i++) {
-    if (a[i].value == "d. An increase in capital investment in the industry")
+    if (a[i].value == "D. An increase in capital investment in the industry")
     alert("You have selected the correct answer");
     //push(1).array
 
